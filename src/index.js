e8455461d90913bf6d18c684b03d5fe7881b289e
PedroRocha2006 <phrf1@aluno.ifnmg.edu.br>
2025-03-14 17:16:17 +0000

    Mensagem de commmit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+//index.js
+import dotenv from "dotenv";
+import express from "express";
+import roteadorUsuario from "./routes/usuario.js";
+import roteadorLogin from "./routes/login.js";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express(); // Instancia o Express
+const port = 3000; // Define a porta
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(roteadorLogin);
+app.use(roteadorUsuario);
+
+app.get("/", (req, res) => {
+  console.log("Rota / solicitada");
+  // Cria a rota da raiz do projeto
+  res.json({
+    nome: "Nicolas", // Substitua pelo seu nome
+  });
+});
+
+app.listen(port, () => {
+  // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
